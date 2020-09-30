b6cfdca7a35daa5e6033de26e3145bd2b07656ef
Satya Bodapati <satya.bodapati@percona.com>
2020-09-30 12:30:48 +0530

    Extract space_id and format the prints

--- innodb_page_header.sh
+++ innodb_page_header.sh
@@ -13,7 +13,7 @@ decode_encryption() {
   echo "MASTER_KEY_ID: $MASTER_KEY_ID ( $((2#$MASTER_KEY_ID)) )"
   SERVER_UUID=$(xxd -p -s10397 -l36 $FILE | xxd -r -p | tr -d '\0')
   echo "SERVER UUID: $SERVER_UUID"
-  echo "Key":
+  echo "Key:"
   xxd -s10433 -l32 $FILE
   echo "iv: "
   xxd -s10465 -l32 $FILE
@@ -31,53 +31,57 @@ decode_flags() {
   S_FSP_FLAGS_WIDTH_SDI=1
   BIT4_MASK=$(($((1<<4)) - 1))
   ANTELOPE=$(($FLAGS & 1))
-  echo "ANTELOPE is $ANTELOPE"
+  echo "ANTELOPE: $ANTELOPE"
   FLAGS=$(($FLAGS >> $S_FSP_FLAGS_WIDTH_POST_ANTELOPE))
   ZIP_SSIZE=$(($FLAGS & $BIT4_MASK))
-  echo "ZIP_SSIZE is $ZIP_SSIZE"
+  echo "ZIP_SSIZE: $ZIP_SSIZE"
   if [ $ZIP_SSIZE != 0 ]; then
    COMPRESSED_PAGE_SIZE=$((512 << $ZIP_SSIZE))
-   echo "COMPRESSED_PAGE_SIZE is $COMPRESSED_PAGE_SIZE"
+   echo "COMPRESSED PAGE_SIZE: $COMPRESSED_PAGE_SIZE"
   else
-   echo "TABLESPACE is NOT ZIP COMPRESSED"
+   echo "COMPRESSED: NO"
   fi
   FLAGS=$(($FLAGS >> $S_FSP_FLAGS_WIDTH_ZIP_SSIZE))
   ATOMIC_BLOBS=$(($FLAGS & 1))
-  echo "ATOMIC_BLOBS is $ATOMIC_BLOBS"
+  echo "ATOMIC_BLOBS: $ATOMIC_BLOBS"
   FLAGS=$(($FLAGS >> $S_FSP_FLAGS_WIDTH_ATOMIC_BLOBS))
   PAGE_SSIZE=$(($FLAGS & $BIT4_MASK))
-  echo "PAGE_SSIZE is $PAGE_SSIZE"
+  echo "PAGE_SSIZE: $PAGE_SSIZE"
   if [ $PAGE_SSIZE != 0 ]; then
    UNCOMPRESSED_PAGE_SIZE=$((512 << $PAGE_SSIZE))
-   echo "UNCOMPRESSED_PAGE_SIZE is $UNCOMPRESSED_PAGE_SIZE"
+   echo "UNCOMPRESSED PAGE_SIZE: $UNCOMPRESSED_PAGE_SIZE"
   else
-   echo "UNCOMPRESSED_PAGE_SIZE is 16384"
+   echo "UNCOMPRESSED PAGE_SIZE: 16384"
   fi
   FLAGS=$(($FLAGS >> $S_FSP_FLAGS_WIDTH_PAGE_SSIZE))
   DATA_DIR=$(($FLAGS & 1))
-  echo "DATADIR is $DATA_DIR"
+  echo "DATADIR: $DATA_DIR"
   FLAGS=$(($FLAGS >> $S_FSP_FLAGS_WIDTH_DATA_DIR))
   SHARED=$(($FLAGS & 1))
-  echo "SHARED_TABLESPACE is $SHARED"
+  echo "SHARED_TABLESPACE: $SHARED"
   FLAGS=$(($FLAGS >> $S_FSP_FLAGS_WIDTH_SHARED))
   TEMPORARY=$(($FLAGS & 1))
-  echo "TEMPORARY TABLESPACE is $TEMPORARY"
+  echo "TEMPORARY TABLESPACE: $TEMPORARY"
   FLAGS=$(($FLAGS >> $S_FSP_FLAGS_WIDTH_TEMPORARY))
   ENCRYPTION=$(($FLAGS & 1))
-  echo "ENCRYPTION is $ENCRYPTION"
+  echo "ENCRYPTION: $ENCRYPTION"
   FLAGS=$(($FLAGS >> $S_FSP_FLAGS_WIDTH_ENCRYPTION))
   SDI=$(($FLAGS & 1))
-  echo "SDI is $SDI"
+  echo "SDI: $SDI"
  
   if [[ $ENCRYPTION -eq "1" ]]; then
     decode_encryption
   fi
 }
 echo "Reading FSP_FLAGS of ${FILE}"
+SPACE_ID=$(od -An -j38 -D -N4 --endian=big $FILE)
+echo "SPACE_ID of tablespace: $SPACE_ID"
+
 echo
 FLAGS=$(xxd -b -s54 -l4 $FILE | awk '{print $2 $3 $4 $5}')
-#other way. Depends on od have --endian option. Not all od versions has it
-#FLAGS=$(od -An -j 54 -d -N4  --endian=big $FILE |awk '{print $2}')
+#other way. Depends on od have --endian option. Not all od versions have it
+#FLAGS=$(od -An -j 54 -D -N4  --endian=big $FILE')// For 4 bytes, use -D
+#LSN=$(od -An -j 16 -L -N8  --endian=big $FILE') // For 8 bytes, use -L
 FLAGS=`echo "ibase=2; $FLAGS" | bc`
 echo "FLAGS: $FLAGS"
 decode_flags $FLAGS
